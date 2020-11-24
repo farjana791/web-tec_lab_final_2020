f21cc402cd39b875977d5c6915947b0108a53b70
Bithi <farjanaislambithi89@gmail.com>
2020-11-24 17:14:12 +0600

    lab1 done

---
+++ Finallab1/php_codes/Registrationvalidation.php
@@ -0,0 +1,98 @@
+<?php
+	$uname="";
+	$err_uname="";
+	$pass="";
+    $err_pass="";
+    $cpass="";
+    $err_cpass="";
+    $type="";
+    $err_type="";
+	$hasError=false;
+	if(isset($_POST["ok"])){
+        //USERNAME VALIDATION
+		if(empty($_POST["uname"])){
+			$err_uname="Username Required";
+			$hasError =true;	
+        }
+        elseif((strlen($_POST["uname"])<6)){
+            $err_uname="Username must be 6 characters long!";
+            $hasError=true;
+        }
+		else{
+			$uname = htmlspecialchars($_POST["uname"]);
+        }
+        //PASSWORD VALIDATION
+		if(empty($_POST["pass"])){
+            $err_pass="Please Enter Password!";
+            $hasError=true;
+        }
+        elseif(strlen($_POST["pass"])<8){
+            $err_pass="Password must be 8 characters long.";
+            $hasError=true;
+        }
+        elseif(!strpos($_POST['pass'], "1") && !strpos($_POST['pass'], "2") && !strpos($_POST['pass'], "3") && !strpos($_POST['pass'], "4")
+            && !strpos($_POST['pass'], "5") && !strpos($_POST['pass'], "6") && !strpos($_POST['pass'], "7") && !strpos($_POST['pass'], "8")
+            && !strpos($_POST['pass'], "9") && !strpos($_POST['pass'], "0")) {
+            $err_pass="Password must have 1 numeric";
+            $hasError=true;
+        }
+        elseif(strcmp(strtoupper($_POST["pass"]),$_POST["pass"])==0 && strcmp(strtolower($_POST["pass"]),$_POST["pass"])==0){
+            $err_pass="Password must contain 1 Upper and Lowercase letter.";
+            $hasError=true;
+        }
+        elseif(strpos($_POST["pass"],"#")==false && strpos($_POST["pass"],"?")==false){
+            $err_pass="Password must contain '#' or '?'.";
+            $hasError=true;
+        }
+        elseif(empty($_POST["cpass"])){
+            $err_cpass="Please Enter Confirm Password!";
+            $hasError=true;
+        }
+        elseif(strcmp($_POST["cpass"],$_POST["pass"])!=0){
+            $err_cpass="Password and Confirm Password must be same.";
+            $hasError=true;
+        }
+        else{
+            $pass=htmlspecialchars($_POST["pass"]);
+        }
+        //CONFIRM PASSWORD VALIDATION
+        
+        //Type VALIDATION
+        if(isset($_POST["type"])){
+            $type=$_POST["type"];
+        }
+        else{
+            $err_type="Select a Type!";
+            $hasError=true;
+        }
+		
+		if(!$hasError){
+            $dbuname="root";
+            $dbserver="localhost";
+            $dbpass="";
+            $dbname="web_tec";
+            $query="";
+
+            $conn=mysqli_connect($dbserver,$dbuname,$dbpass,$dbname);
+            if(!$conn){
+                die("Connection Failed: ".mysqli_connect_error());
+            }
+
+            $p=$pass;
+            $pt=md5($pass);
+			echo $p;
+
+            if(strcmp($type,"User")==0){
+                $query="INSERT INTO users VALUES('".$uname."','".$p."','user')";
+                mysqli_query($conn,$query);
+                header("Location: login.php");
+            }
+            elseif(strcmp($type,"Admin")==0){
+                $query="INSERT INTO users VALUES('".$uname."','".$p."','admin')";
+                mysqli_query($conn,$query);
+                header("Location: login.php");
+            }
+		}
+	}
+	
+?>
